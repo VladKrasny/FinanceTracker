33608a7c0437ede365a22f9b6d6cb6d48ee905d9
janulewicz-and <and.yanulevich@gmail.com>
2025-12-22 15:02:33 +0100

    Category selection should be optional when creating a transaction (#21)
    * Make category selection optional in TransactionForm and update validation in CategoryInput
    
    ---------
    
    Co-authored-by: Andrzej Janulewicz <and.janulewicz@gmail.com>

--- src/components/CategoryInput.vue
+++ src/components/CategoryInput.vue
@@ -11,9 +11,18 @@
 
 <script>
 import TheInput from "./TheInput.vue";
+
 export default {
   name: "CategoryInput",
   components: { TheInput },
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: ["modelValue", "error"],
 
   data() {
     return {
@@ -31,24 +40,14 @@ export default {
     },
   },
   watch: {
-    categotryError(value) {
+    categoryError(value) {
       this.$emit("error", value);
     },
 
     model(value) {
       const val = value.trim();
 
-      if (val === "") {
-        this.categoryError = "Category cannot be empty";
-        return;
-      }
-
-      if (val.length < 3) {
-        this.categoryError = "Category must be at least 3 characters long";
-        return;
-      }
-
-      if (!/^[A-Za-z\s]+$/.test(val)) {
+      if (val.length > 0 && !/^[A-Za-z\s]+$/.test(val)) {
         this.categoryError = "Category must contain only letters and spaces";
         return;
       }
