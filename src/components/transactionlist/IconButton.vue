12be29143cd32c6150734b63eb96d339f703d0f3
Andrzej Janulewicz <and.janulewicz@gmail.com>
2025-12-25 12:04:56 +0100

    fix: show empty state and delete items related to removed category

---
+++ src/components/transactionlist/IconButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    :class="{
+      'icon-button': true,
+      [`icon-button--${variant}`]: Boolean(variant),
+    }"
+    @click="$emit('click')"
+  >
+    <span class="material-symbols-outlined">
+      {{ iconSymbol }}
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "IconButton",
+  emits: ["click"],
+  props: {
+    variant: {
+      type: String,
+      default: "",
+    },
+    iconSymbol: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.icon-button {
+  color: gray;
+  height: 40px;
+  background-color: inherit;
+  border-width: 0px;
+  cursor: pointer;
+}
+.icon-button--edit {
+  color: #848c95;
+}
+.icon-button--delete {
+  color: #dc3545;
+}
+</style>
